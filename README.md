14adec6ab2a1c088bcc5eb25a82721acee1f5d5b
andriy <and123bar@gmail.com>
2025-09-03 23:44:04 +0300

    clean some coments

---
+++ README.md
@@ -0,0 +1,13 @@
+- Header - Бурдюжа Григорій
+- Hero - Alina Voitseshyna
+- Welcome - Vlad Khoroshunov
+- Quality - Руслан Линник
+- Experience - Hanna Muzychuk
+- Subscribe - Aleksey_Skom
+- Testimonials - Aleksey_Skom
+- Location - Vladyslav Shevchuk
+- Footer - Андрій Ткаченко
+- Mobile menu - Бурдюжа Григорій
+- Modal - Aleksey_Skom
+
+team-lead - Andriy Baranovich
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,43 +1,22 @@
-// (() => {
-//   const refs = {
-//     // Додати атрибут data-modal-open на кнопку відкриття
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     // Додати атрибут data-modal-close на кнопку закриття
-//     closeModalBtns: document.querySelectorAll('[data-modal-close]'),
-//     // Додати атрибут data-modal на бекдроп модалки
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   // refs.closeModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtns.forEach(btn => {
-//     btn.addEventListener('click', toggleModal);
-//   });
-
-//   function toggleModal(event) {
-//     if (event) event.preventDefault(); // отменяем submit/перезагрузку
-//     refs.modal.classList.toggle('is-open'); // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-//   }
-// })();
 
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtns: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    form: document.querySelector('form'), // ⚡ ссылка на форму
+    form: document.querySelector('form'), 
   };
 
   refs.openModalBtn.addEventListener('click', function (event) {
-    event.preventDefault(); // отключаем автоотправку формы
+    event.preventDefault(); 
 
-    // ✅ используем стандартную проверку формы
+
     if (!refs.form.checkValidity()) {
-      refs.form.reportValidity(); // покажет встроенные подсказки браузера
-      return; // если не валидно → не открываем модалку
+      refs.form.reportValidity(); 
+      return; 
     }
 
-    toggleModal(); // если форма валидна → открываем
+    toggleModal(); 
   });
 
   refs.closeModalBtns.forEach(btn => {
